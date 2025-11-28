ea9bd7f8879982a08ba66d5487906fcdf53378bb
MRegisCT <maregina861@gmail.com>
2025-11-28 13:12:41 -0600

    firts commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  spotify: {
+    CLIENT_ID: '5b267abe170f468f847b57d7d78cef95',
+    CLIENT_SECRET: 'e9802e8d9cad40fa9d9c076d8ff8e7cc',
+    AUTH_API_URL: 'https://accounts.spotify.com/api/token',
+    apiUrl: 'https://api.spotify.com/v1',
+    redirectUri: 'https://tu-dominio.com'
+  }
+};
